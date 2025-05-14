d31902354291212649dc612746601caf693cdc4b
Fred <gasolin@gmail.com>
2025-05-14 21:58:21 +0800

    improvement - add bare detection bash

--- bin/main.sh
+++ bin/main.sh
@@ -1,3 +1,14 @@
-#!/usr/bin/env bare
+#!/bin/
 
-bare ../index.js
+# Check if the bare command is available
+if ! command -v bare &> /dev/null
+then
+    echo "Error: bare command not found." >&2
+    echo "Please install bare globally using npm:" >&2
+    echo "npm i -g bare" >&2
+    # Exit with a non-zero status code to indicate an error
+    exit 1
+fi
+
+# "$@" will pass any arguments given to the bin command
+bare ../index.js "$@"

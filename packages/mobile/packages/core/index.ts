abe0ad2137161f21ad87cb3f3a4771f6d572d66a
Fred <gasolin@gmail.com>
2025-12-29 17:07:52 +0800

    fix core/index

--- packages/mobile/packages/core/index.ts
+++ packages/mobile/packages/core/index.ts
@@ -8,24 +8,6 @@ export const initBareKit = async (setResponse) => {
   const source = require('./main.bundle')
   const worklet = new Worklet()
 
-  // const source = `
-  //   const { IPC: rawIPC } = BareKit
-  //   const RPC = require('tiny-buffer-rpc')
-  //   const FramedStream = require('framed-stream')
-  //   const c = require('compact-encoding')
-  //   const framed = new FramedStream(rawIPC)
-  //   const rpc = new RPC(data => framed.write(data))
-  //   framed.on('data', data => rpc.recv(data))
-  //   rpc.register(1, {
-  //     request: c.string,
-  //     response: c.string,
-  //     onrequest: (msg) => {
-  //       console.log('Worklet received:', msg)
-  //       return 'Hello from Bare RPC!'
-  //     }
-  //   })
-  // `
-
   worklet.start('barechat:/main.bundle', source)
 
   // React Native side RPC setup
@@ -44,11 +26,6 @@ export const initBareKit = async (setResponse) => {
   } catch (err) {
     console.error('RPC Error:', err)
   }
-
-  // const { IPC } = worklet
-
-  // IPC.on('data', (data: Uint8Array) => setResponse(b4a.toString(data)))
-  // IPC.write(b4a.from('>>>>>>>>>>>>> Hello from React Native!'))
 }
 
 export default Worklet
